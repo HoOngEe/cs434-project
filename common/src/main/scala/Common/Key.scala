8e328472c99ad03014d9bacc8c23559ece2db3bd
Seonpyo Kim <tomandjerry@postech.ac.kr>
2020-11-16 19:01:10 +0900

    Fix #1

--- common/src/main/scala/Common/Key.scala
+++ common/src/main/scala/Common/Key.scala
@@ -1,27 +1,19 @@
 package Common
 
-class Key(private val byteList: List[Byte]) {
+class Key(private val byteList: List[Byte]) extends Ordered[Key] {
   require(byteList.length == 10)
 
-  private object KeyOrdering extends Ordering[Key] {
-    override def compare(x: Key, y: Key): Int = {
-      def compare_aux(x: List[Byte], y: List[Byte]): Int = {
-        (x, y) match {
-          case (List(), List()) => 0
-          case (xFst::xTail, yFst::yTail) => {
-            val fstCompared = xFst compare yFst
-            if (fstCompared == 0) compare_aux(xTail, yTail) else fstCompared
-          }
-          case _ => throw new IllegalArgumentException("lengths must be same")
+  override def compare(other: Key): Int = {
+    def compare_aux(x: List[Byte], y: List[Byte]): Int = {
+      (x, y) match {
+        case (List(), List()) => 0
+        case (xFst::xTail, yFst::yTail) => {
+          val fstCompared = xFst compare yFst
+          if (fstCompared == 0) compare_aux(xTail, yTail) else fstCompared
         }
+        case _ => throw new IllegalArgumentException("lengths must be same")
       }
-      compare_aux(x.byteList, y.byteList)
     }
+    compare_aux(this.byteList, other.byteList)
   }
-
-  @inline def <(other: Key): Boolean = KeyOrdering.compare(this, other) < 0
-  @inline def >(other: Key): Boolean = KeyOrdering.compare(this, other) > 0
-  @inline def ==(other: Key): Boolean = KeyOrdering.compare(this, other) == 0
-  @inline def >=(other: Key): Boolean = this > other || this == other
-  @inline def <=(other: Key): Boolean = this < other || this == other
 }
